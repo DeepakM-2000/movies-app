85c626b8425cdd9a00e364bc1771b4d227ea700c
Deepak <mauryadeepak146@gmail.com>
2022-09-16 13:58:04 +0530

    Creating header,home,material ui component

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -4,8 +4,7 @@ import "./Header.css"
 const Header = () => {
   return (
     <div className='Header'>
-      Header
-      <img src={logo} alt="logo"/>
+    <img className='headerlogo' src={logo} alt="logo"/>
     </div>
   )
 }
